7ea41a422b5e010ba6417296e019fbdf6f877702
Fabrizio Destro <destro.fabrizio@gmail.com>
2018-08-14 17:55:56 +0200

    Changed sparse-checkout command to be more robust.

--- git/git-commands/git-sparse-checkout.sh
+++ git/git-commands/git-sparse-checkout.sh
@@ -1,25 +1,39 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 2 ]; then
-	echo "Illegal number of arguments, please specify a repository and a directory."
+usage() {
+	cat << EOU
+git sparse-checkout
+
+Usage:
+	sparse-checkout <repository> <dirs>...
+
+Arguments
+	repository is the git repository uri
+	dirs are all repository's directorires to checkout
+EOU
+}
+
+if ! command -v docopts >/dev/null 2>&1; then
+	echo "docopts is not installed"
 	exit -1
 fi
 
+# processing arguments
+eval "$(docopts -h "$(usage)" : "$@")"
+
 working_directory="$(pwd)"
 temp_directory=$(mktemp -d)
-
-# Reading arguments
-git_repository=$1
-shift
+repository_name=$(basename -s .git "$repository")
+target_directory="${working_directory}/${repository_name}"
 
 cd "$temp_directory"
 
-git init
-git remote add origin $git_repository
+git init --quiet
+git remote add origin $repository
 git config core.sparseCheckout true
 
-printf "%s\n" $@ > .git/info/sparse-checkout
+printf "%s\n" "${dirs[@]}" > .git/info/sparse-checkout
+git pull --depth 1 origin master
 
-git pull origin master
-
-mv * "$working_directory"
+mkdir "${target_directory}"
+mv * "${target_directory}"
