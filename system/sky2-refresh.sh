b0d90afca10b82fd10a4f17811ea33503cd62e37
Fabrizio Destro <destro.fabrizio@gmail.com>
2018-11-18 21:25:14 +0100

    Added script to refresh the ethernet driver.

---
+++ system/sky2-refresh.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# see https://askubuntu.com/a/1030154/853462
+
+PROGNAME=$(basename "$0")
+state=$1
+action=$2
+
+function log {
+    logger -i -t "$PROGNAME" "$*"
+}
+
+log "Running $action $state"
+
+if [[ $state == post ]]; then
+    modprobe -r sky2 \
+    && log "Removed sky2" \
+    && modprobe -i sky2 \
+    && log "Inserted sky2"
+fi
