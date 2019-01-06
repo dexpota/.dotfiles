64b52d9905d29be93dd23c6d705991eb175804f7
Fabrizio Destro <destro.fabrizio@gmail.com>
2019-01-06 11:51:12 +0000

    bash: removed debugging echo

--- bash/.bash-it-custom/plugins/dircolors.plugin.bash
+++ bash/.bash-it-custom/plugins/dircolors.plugin.bash
@@ -5,7 +5,6 @@
 if command -v dircolors 1>/dev/null 2>&1; then
 	# if the ".dircolors" directory exists we load the colors from it,
 	# otherwise dircolors uses its predefined database
-	echo Here
 	test -r ~/.dircolors \
 		&& eval "$(dircolors -b ~/.dircolors)" \
 		|| eval "$(dircolors -b)"
