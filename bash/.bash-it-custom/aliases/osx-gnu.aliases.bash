2d6e40e1b5fa97f82dc4f74e03143408dec3c961
Fabrizio Destro <destro.fabrizio@gmail.com>
2019-01-06 15:30:03 +0000

    Fixed issue with osx aliases.

--- bash/.bash-it-custom/aliases/osx-gnu.aliases.bash
+++ bash/.bash-it-custom/aliases/osx-gnu.aliases.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-[ "$OSTYPE" -ne "darwin*" ] && return
+[ "$OSTYPE" != "darwin*" ] && return
 
 command -v ghead 1>/dev/null 2>&1 && alias head=ghead
 command -v gmktemp 1>/dev/null 2>&1 && alias mktemp=gmktemp
