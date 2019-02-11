ecd6f994155e551bee15508f812987d1f0dddf1f
Fabrizio Destro <destro.fabrizio@gmail.com>
2019-02-11 20:34:53 +0100

    script: updated script to generate resources.

--- scripts/android-resource.sh
+++ scripts/android-resource.sh
@@ -26,11 +26,103 @@
 # TODO aggiungere profili per tipologie di immagini, ad esempio un'icon con
 # risoluzione di 1024x1024 non ha molto senso
 
+# Configuration
+configure() {
+	import_utilites=false
+	# exits the script if you try to use uninitialized variables
+	set -o nounset
+	# exits the script if any statement returns a non-true return value
+	# if you are willing to continue when a statement fails you can use this
+	# construct: command || true
+	set -o errexit
+}
+
 usage() {
 	cat << EOU
 android-resource
 
-Usage:	android-generate-resources <drawable> (-ldpi | -mdpi | -hdpi | - xhdpi | -xxhdpi | -xxxhdpi) [--generate-dpi=<dpis>]
+Usage:
+	android-generate-resources <drawable> (--ldpi | --mdpi | --hdpi | --xhdpi | --xxhdpi | --xxxhdpi) [--generate-dpi=<dpis>] [options]
 
+Options:
+	-v,--verbose  Output more information
+	-d,--debug  Enable debug mode, output even more informations
 EOU
 }
+
+verbose() {
+	[ $verbose = "true" ]
+}
+
+logverbose() {
+	if verbose; then
+		echo "${@}"
+	fi
+}
+
+debug() {
+	[ $debug = "true" ]
+}
+
+import_utilities() {
+	# if we want to import common utilities
+	if [ "$import_utilites" == true ]; then
+
+		# if an env variable is not defined try a default location
+		if [ -z $SCRIPTING_UTILITIES ]; then
+			scripting_utilities=$HOME/.script/libs/utilities.sh
+		fi
+
+		# Source Scripting Utilities
+		if [ -f "$scripting_utilites" ]; then
+			source "$scripting_utilities"
+		else
+			echo "Scripting utilities not found!"
+			echo "$scripting_utilities doesn't seem to exists."
+			exit -1
+		fi
+	fi
+}
+
+# This function will be called when the script exits or when one of TERM or
+# INT signal is received.
+cleanup() {
+	# your cleaning code here ...
+	exit -1
+}
+
+initialize() {
+	import_utilities
+
+	# trap bad exits with the cleanup function
+	trap cleanup EXIT INT TERM
+
+	# run bash in debugging mode
+	if debug; then
+		set -x
+	fi
+}
+
+main() {
+	configure
+	initialize
+
+	if verbose; then
+		echo "Verbose mode..."
+		echo
+	elif debug; then
+		echo "Debug mode..."
+		echo
+	fi
+
+	if $ldpi; then
+		logverbose "Generating resources for image file @ ldpi"
+	fi
+
+}
+
+# check if script is being executed
+if [ "$0" =  "$BASH_SOURCE" ]; then
+	eval "$(docopts -h "$(usage)" : "$@")"
+	main
+fi
