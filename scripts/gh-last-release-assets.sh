03274238d1f853a52f53cb718758b3b41f333a80
Fabrizio Destro <destro.fabrizio@gmail.com>
2019-02-25 23:25:27 +0100

    scripts: structuring the script.

--- scripts/gh-last-release-assets.sh
+++ scripts/gh-last-release-assets.sh
@@ -1,7 +1,6 @@
-
 #!/usr/bin/env bash
 #
-# @author 		Fabrizio Destro
+# @author		Fabrizio Destro
 # @copyright	Copyright 2018, Fabrizio Destro
 # @license
 #  This work is licensed under the terms of the MIT license.
@@ -9,29 +8,38 @@
 
 usage() {
 	cat << EOU
-gh-last-release-assets
+gh-last-release-assets downloads all assets belonging to the last release of a
+given github repository.
 
 Usage:
-	gh-last-release-assets <github_path> <target_directory>
+	gh-last-release-assets <github_repository> <target_directory>
 EOU
 }
 
-eval "$(docopts -h "$(usage)" : "$@")"
+main() {
+	github_api_endpoint="https://api.github.com/repos/$github_repository/releases/latest"
 
-github_api_endpoint="https://api.github.com/repos/$github_path/releases/latest"
+	response=$(curl -w '\n%{http_code}' -s -o - $github_api_endpoint)
 
-response=$(curl -w '\n%{http_code}' -s -o - $github_api_endpoint) 
+	response_body=$(echo "$response" | head -n -1)
+	response_code=$(echo "$response" | tail -1)
 
-response_body=$(echo "$response" | head -n -1)
-response_code=$(echo "$response" | tail -1)
+	# TODO target_directory should be optional and if not set should be the current
+	# one
+	# target_directory="."
 
-# TODO target_directory should be optional and if not set should be the current
-# one
-# target_directory="."
+	if [[ $response_code == 200 ]]; then
+		{
+			cd "$target_directory"
+			echo $response_body \
+				| jq -r ".assets[] | \"\(.browser_download_url) \(.name) \"" \
+				| parallel --colsep " " --no-notice -j+1 curl -L {1} -o {2}
+		}
+	fi
+}
 
-if [[ $response_code == 200 ]]; then
-	{
-		cd "$target_directory"
-		echo $response_body | jq -r ".assets[] | \"\(.browser_download_url) \(.name) \"" | parallel --colsep " " --no-notice -j+1 curl -L {1} -o {2}
-	}
+# check if script is being executed
+if [[ $_ == $0 ]]; then
+	eval "$(docopts -h "$(usage)" : "$@")"
+	main
 fi
