cf64fec4f6433b14e0cb8e56f9c01c3f5ed1fef0
Fabrizio Destro <destro.fabrizio@gmail.com>
2019-02-25 23:33:36 +0100

    script: download a single asset from the latest release of a github repo.

---
+++ scripts/gh-last-release-asset.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# @author       Fabrizio Destro
+# @copyright    Copyright 2018, Fabrizio Destro
+# @license
+#  This work is licensed under the terms of the MIT license.
+#  For a copy, see <https://opensource.org/licenses/MIT>.
+
+usage() {
+    cat << EOU
+gh-last-release-asset download the specified asset belonging to the last
+release of a given github repository.
+
+Usage:
+    gh-last-release-assets <github_repository> <asset> <target_directory>
+EOU
+}
+
+main() {
+    github_api_endpoint="https://api.github.com/repos/$github_repository/releases/latest"
+
+    curl -s "$github_api_endpoint" \
+        | jq -r ".assets[].browser_download_url" \
+        | grep "$asset" \
+        | wget -i -
+
+}
+
+# check if script is being executed
+if [[ $_ == $0 ]]; then
+    eval "$(docopts -h "$(usage)" : "$@")"
+    main
+fi
