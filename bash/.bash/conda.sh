ea3ae1c2a1b738a1d860c453af6967d57d192508
Fabrizio Destro <7031675+dexpota@users.noreply.github.com>
2019-09-08 12:07:47 +0200

    Move environment setup outside conda script

--- bash/.bash/conda.sh
+++ bash/.bash/conda.sh
@@ -1,13 +1,17 @@
-CONDA_ROOT=$HOME/miniconda3
+# Conda environment initalization script with some additional tweaks. Set the
+# environment variable ENV_CONDA_ROOT to add conda. This script must be
+# sourced.
 
-__conda_setup=$("$CONDA_ROOT/bin/conda" 'shell.bash' 'hook' 2> /dev/null)
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "$CONDA_ROOT/etc/profile.d/conda.sh" ]; then
-        . "$CONDA_ROOT/etc/profile.d/conda.sh"
-    else
-        export PATH="$CONDA_ROOT/miniconda3/bin:$PATH"
-    fi
+if [ -z "$ENV_CONDA_ROOT" ]; then
+	if __conda_setup=$("$ENV_CONDA_ROOT/bin/conda" 'shell.bash' 'hook' 2> /dev/null); then
+		eval "$__conda_setup"
+	else
+		if [ -f "$ENV_CONDA_ROOT/etc/profile.d/conda.sh" ]; then
+			# shellcheck source=/dev/null
+			. "$ENV_CONDA_ROOT/etc/profile.d/conda.sh"
+		else
+			export PATH="$ENV_CONDA_ROOT/bin:$PATH"
+		fi
+	fi
+	unset __conda_setup
 fi
-unset __conda_setup
