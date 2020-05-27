677cc9308847bbc75b70aaa5f2624b726cd19bd0
Fabrizio <7031675+dexpota@users.noreply.github.com>
2020-05-27 09:02:38 +0200

    Update osx-aliases.sh

--- bash/.bash/osx-aliases.sh
+++ bash/.bash/osx-aliases.sh
@@ -1,2 +1,3 @@
 command -v ghead 1>/dev/null 2>&1 && alias head=ghead
 command -v gmktemp 1>/dev/null 2>&1 && alias mktemp=gmktemp
+alias dns-refresh=dscacheutil -flushcache && sudo killall -HUP mDNSResponder
