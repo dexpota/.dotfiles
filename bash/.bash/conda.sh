54e0637ea0b1537b13f3cdecfaf42b3b82270442
Fabrizio Destro <7031675+dexpota@users.noreply.github.com>
2020-06-04 23:04:26 +0200

    Update conda configuration

--- bash/.bash/conda.sh
+++ bash/.bash/conda.sh
@@ -3,6 +3,13 @@
 # sourced.
 
 if [ ! -z "$ENV_CONDA_ROOT" ]; then
+	# See https://github.com/conda/conda/issues/5388
+	export CONDA_ENVS_DIRS=$HOME/.config/conda/envs
+	export CONDA_PKGS_DIRS=$HOME/.config/conda/pkgs
+
+	# See https://docs.conda.io/projects/conda/en/latest/user-guide/configuration/use-condarc.html#searching-for-condarc
+	export CONDARC=$HOME/.config/conda/condarc
+
 	if __conda_setup=$("$ENV_CONDA_ROOT/bin/conda" 'shell.bash' 'hook' 2> /dev/null); then
 		eval "$__conda_setup"
 	else
