6cabb3b8b5431340980f0ac36a5644ff0d010515
Fabrizio Destro <7031675+dexpota@users.noreply.github.com>
2020-09-07 13:37:34 +0200

    Add git command to delete stale branches

--- git/git-commands/git-delete-stale.sh
+++ git/git-commands/git-delete-stale.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+# TODO add tests
 LANG=en git branch --format='%(if:equals=gone)%(upstream:track,nobracket)%(then)%(refname:short)%(end)' | grep '.' | grep -v "\*"  | xargs git branch -d
