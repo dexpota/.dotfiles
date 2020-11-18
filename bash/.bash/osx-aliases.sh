55de4e3617fdc69b9324a6dc531bbb09fe13348a
Fabrizio Destro <7031675+dexpota@users.noreply.github.com>
2020-11-18 15:49:13 +0100

    Fix dns-refresher

--- bash/.bash/osx-aliases.sh
+++ bash/.bash/osx-aliases.sh
@@ -1,6 +1,8 @@
 command -v ghead 1>/dev/null 2>&1 && alias head=ghead
 command -v gmktemp 1>/dev/null 2>&1 && alias mktemp=gmktemp
-alias dns-refresh=dscacheutil -flushcache && sudo killall -HUP mDNSResponder
+
+# Clean local DNS cache. Found on the Internet, I think it does not always work.
+alias dns-refresh="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 
 # Start VS Code from the terminal
 alias code="open -a 'Visual Studio Code'"
