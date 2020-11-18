f29c7e0fb1c252b6270c6817e9e5385773cbca44
Fabrizio Destro <7031675+dexpota@users.noreply.github.com>
2020-11-18 15:44:31 +0100

    Add code alias on OSX

--- bash/.bash/osx-aliases.sh
+++ bash/.bash/osx-aliases.sh
@@ -1,3 +1,6 @@
 command -v ghead 1>/dev/null 2>&1 && alias head=ghead
 command -v gmktemp 1>/dev/null 2>&1 && alias mktemp=gmktemp
 alias dns-refresh=dscacheutil -flushcache && sudo killall -HUP mDNSResponder
+
+# Start VS Code from the terminal
+alias code="open -a 'Visual Studio Code'"
